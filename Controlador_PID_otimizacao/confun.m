4990f53ed3bd25fda56213f4584ac0f46a500bc8
Francinildo-Figueiredo <francinildofigueredo@gmail.com>
2023-05-11 00:03:44 -0300

    att

--- Controlador_PID_otimizacao/confun.m
+++ Controlador_PID_otimizacao/confun.m
@@ -1,10 +1,11 @@
 function [C,Ceq]=confun(x,s,G,MS_max,MT_max, Ju_max)
     Kp=x(1); Ki=x(2); Kd=x(3);
     K=Kp + Ki/s + Kd*s/(1+0.01*s); % Controlador PID
-    MS=norm(feedback(1,G*K),inf);                 % Margem de estabilidade
-    MT=norm(feedback(G*K,1),inf);                 %
-    Ju=norm(feedback(K, G),inf);
-    stab=norm(feedback(G*K,1)); % Norma 2 da função de sensibilidade complementar
+    MS=norm(feedback(1,pade(G)*K),inf);                 % Margem de estabilidade
+    MT=norm(feedback(pade(G)*K,1),inf);                 %
+    Ju=norm(feedback(K, pade(G)),inf);
+    %Jv=norm(feedback(pade(G)/s,K),inf);
+    stab=norm(feedback(pade(G)*K,1)); % Norma 2 da função de sensibilidade complementar
     if stab<inf, C=[MS-MS_max; MT-MT_max; Ju-Ju_max];
     else C=[1; 1; 1]; end
     Ceq=[];
--- Controlador_PID_otimizacao/plantas_aproximadas.m
+++ Controlador_PID_otimizacao/plantas_aproximadas.m
@@ -1,13 +1,21 @@
 % Equivalente de segunda ordem com atraso
 
-s=tf('s'); G=1/((1+s)*(1+0.5*s)*(1+0.25*s));
+s=tf('s'); 
+%G=1/((1+s)*(1+0.5*s)*(1+0.25*s)); % G1
+%G=1/(1+s)^3;                      % G2
+%G=exp(-0.3*s)/((1+s)*(1+0.5*s));  % G3
+G=1/(s*(1+s)*(1+0.2*s));
 %G = exp(-s)/(s+1)^4;
 % figure(1);
 % step(feedback(G,1));
 
 % 1ª ordem
 T = 0;
-taui = [1, 0.5, 0.25];
+%taui = [1, 0.5, 0.25]; % G1
+%taui = [1, 1, 1];      % G2
+% taui = [1, 0.5, 0];   % G3
+taui = [1, 0.2, 0];    %G4
+%taui = [1,1,1,1];
 theta0 = 0;
 K = 1;
 tau1 = taui(1) + taui(2)/2;
@@ -15,6 +23,7 @@ theta = theta0 + taui(2)/2 + sum(taui(3:end)) + sum(T);
 G1a = K*exp(-theta*s)/(tau1*s + 1);
 
 % Calculando os parâmetros do controlador PI de acordo com o SIMC
+tau_c = theta;
 Kp1 = (1/K)*(tau1/(tau_c+theta));
 Ti1 = min(tau1, 4*(tau_c + theta));
 Ki1 = Kp1/Ti1;
@@ -26,7 +35,11 @@ H1 = feedback(G*Kpi,1);
 
 % 2ª ordem
 T = 0;
-taui = [1, 0.5, 0.25];
+%taui = [1, 0.5, 0.25]; % G1
+%taui = [1, 1, 1];      % G2
+% taui = [1, 0.5, 0];   % G3
+taui = [1, 0.2, 0];    %G4
+%taui = [1,1,1,1];
 theta0 = 0;
 K = 1;
 tau1 = taui(1);
@@ -35,6 +48,7 @@ theta = theta0 + taui(3)/2 + sum(taui(4:end)) + sum(T);
 G2a = K*exp(-theta*s)/((tau1*s + 1)*(tau2*s + 1));
 
 % Calculando os parâmetros do controlador PID de acordo com o SIMC
+tau_c = theta;
 Kp2 = (1/K)*(tau1/(tau_c+theta));
 Ti2 = min(tau1, 4*(tau_c + theta));
 Td2 = tau2;
@@ -48,15 +62,16 @@ step(H2);
 hold on;
 
 % Criterios antes da otimização
-MSb=norm(feedback(1,G*Kpid2),inf)
-MTb=norm(feedback(G*Kpid2,1),inf)
-Jvb=norm(feedback(G/s,Kpid2),inf)
-Jub=norm(feedback(Kpid2, G),inf)
+MSb=norm(feedback(1,pade(G)*Kpid2),inf)
+MTb=norm(feedback(pade(G)*Kpid2,1),inf)
+Jvb=norm(feedback(pade(G)/s,Kpid2),inf)
+Jub=norm(feedback(Kpid2, pade(G)),inf)
 
 % 
-MS_max=1.7;
+MS_max=1.8;
 MT_max=1.3;
-Ju_max=250;
+%Jv_max=0.66;
+Ju_max=20;
 x = [Kp2, Ki2, Kd2];
 
 options = optimset('Algorithm','active-set');
@@ -68,12 +83,15 @@ Kpid = Kp + Ki/s + Kd*s/(1+0.01*s);
 H = feedback(G*Kpid,1);
 
 %Critérios após a otimização
-Jv=norm(feedback(G/s,Kpid),inf)
-Ju=norm(feedback(Kpid, G),inf)
-MS=norm(feedback(1,G*Kpid),inf)
-MT=norm(feedback(G*Kpid,1),inf)
+Jv=norm(feedback(pade(G)/s,Kpid),inf)
+Ju=norm(feedback(Kpid, pade(G)),inf)
+MS=norm(feedback(1,pade(G)*Kpid),inf)
+MT=norm(feedback(pade(G)*Kpid,1),inf)
 step(H);
-legend('Não otimizado', 'Otimizado');
+stepinfo(H)
+title('Planta G_3');
+legend('Controlador SIMC', 'Controlador otimizado', 'location', 'best','FontSize', 10);
+grid on;
 
 
 
