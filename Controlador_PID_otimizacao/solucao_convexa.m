59b959775f8c5d73499a136ebd44204c369aaec9
Francinildo-Figueiredo <66557567+Francinildo-Figueiredo@users.noreply.github.com>
2023-05-23 11:57:00 -0300

    att

--- Controlador_PID_otimizacao/solucao_convexa.m
+++ Controlador_PID_otimizacao/solucao_convexa.m
@@ -1,18 +1,35 @@
-Kp = 0:0.1:15;
-Ki = 0:0.1:15;
-Jv = zeros(1, 2*length(Kp));
+Kp = 1e-2:1e-2:0.5;
+Ki = 1e-2:1e-2:0.5;
+Jv = zeros(length(Kp), length(Ki));
 
 s = tf('s');
-G=1/((1+s)*(1+0.5*s)*(1+0.25*s));
+%G=1/((1+s)*(1+0.5*s)*(1+0.25*s));
+G = 1/(1+s)^3;
 
-k = 1;
+MS_max=1.5;
+MT_max=1.3;
+%Jv_max=0.66;
+Ju_max=20;
+
 for i = 1:1:length(Kp)
     for j = 1:1:length(Ki)
-        Jv(k) = objfun([Kp(i), Ki(j), 0] , s, G);
-        k = k + 1;
-        disp(k);
+        x0 = [Kp(i), Ki(j), 0];
+        options = optimset('Algorithm','active-set');
+        x = fmincon(@(x0) objfun(x0,s,G),x0,[],[],[],[],...
+        [], [], @(x0)confun(x0,s,G,MS_max,MT_max,Ju_max), options);
+        Jv(i,j) = objfun([x(1), x(2), x(3)] , s, G);
+        disp(i);
     end
 end
 
+%[X,Y] = meshgrid(Kp,Ki);
+%Jv = Jv/max(max(Jv));
+%Jv = mag2db(Jv);
+surf(Kp, Ki, Jv);
+%zlim([0 1]);
+xlabel('Kp');
+ylabel('Ki');
+zlabel('Jv');
+grid on;
 
     
