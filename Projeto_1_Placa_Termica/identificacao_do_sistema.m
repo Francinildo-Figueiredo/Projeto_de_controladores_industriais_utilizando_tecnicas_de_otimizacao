ec6416216d81012db7702fb2ac90a0a480e85f50
Francinildo-Figueiredo <66557567+Francinildo-Figueiredo@users.noreply.github.com>
2023-05-30 10:53:49 -0300

    att

---
+++ Projeto_1_Placa_Termica/identificacao_do_sistema.m
@@ -0,0 +1,13 @@
+dobtidos = load('exp23052023115431.mat');
+pv1_aq = dobtidos.pv1(275:667);
+mv1_aq = dobtidos.mv1(275:667);
+duty_op = dobtidos.mv1(250);
+t = 1:1:length(dobtidos.pv1(1:end-1));
+
+[K, T, L] = parametrosFOPTD(pv1_aq - pv1_aq(1), mv1_aq(1) - duty_op, t(end) - t(end-1));
+G = tf(K, [T 1], 'iodelay', L);
+t = 0:1:length(pv1_aq)-1;
+Sis_id = lsim(G, mv1_aq - duty_op, t);
+
+figure(1);
+plot(t,pv1_aq, t, Sis_id+pv1_aq(1));
